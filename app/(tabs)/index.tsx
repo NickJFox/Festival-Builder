f08364f4fe2d663e9a8c5ca016d5c89587ea7bbb
Nick Fox <Nicholasfox4@gmail.com>
2024-02-08 10:30:34 -0600

    Initial set up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,19 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '@/components/EditScreenInfo';
+import { StyleSheet, Pressable } from 'react-native';
+import { Link } from 'expo-router';
 import { Text, View } from '@/components/Themed';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Build Your Own Festival</Text>
+      <Link href="/(tabs)/lineup" asChild>
+      <Pressable>
+        <Text>Small Festival</Text>
+        <Text>Medium Festival</Text>
+        <Text>Large Festival</Text>
+      </Pressable>
+    </Link>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
